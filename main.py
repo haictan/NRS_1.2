31de7cd83b6f9073432d4737a58ac441981487a3
haict-sh <haict-sh@outlook.com>
2024-09-22 19:37:12 +0800

    change server port

--- main.py
+++ main.py
@@ -31,6 +31,6 @@ menu.register_callbacks(app)
 workspace.register_callbacks(app)
 
 if __name__ == '__main__':
-    #app.run_server(host='192.168.1.104', port=8866, debug=False)
-    app.run_server(debug=True)
+    app.run_server(host='127.0.0.1', port=8080, debug=False)
+    #app.run_server(debug=True)
     #app.run_server()
